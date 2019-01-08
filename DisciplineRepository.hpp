e35e3ddc8f57dd197b302ca4319e0a8ea33e370d
MariusMsw <38507154+MariusMsw@users.noreply.github.com>
2019-01-08 03:28:54 +0200

    Delete DisciplineRepository.hpp

--- DisciplineRepository.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Repository.hpp"
-#include "Discipline.hpp"
-
-class DisciplineRepository : public Repository<Discipline> {
-public:
-	Discipline *findByName(std::string name);
-};
